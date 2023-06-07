7267e4bb074355694d87ab6a09d78743edb8628a
Archie <mamchicartem1@gmail.com>
2023-06-07 16:36:55 +0300

    first commit

---
+++ my-app/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+
+export default function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
